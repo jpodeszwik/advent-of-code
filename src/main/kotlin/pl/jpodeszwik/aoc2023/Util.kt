c49068b856355655e64e13c7d455c8fd65122013
Jakub Podeszwik <jakubpodeszwik@gmail.com>
2023-12-13 13:09:54 +0100

    Day 13

--- src/main/kotlin/pl/jpodeszwik/aoc2023/Util.kt
+++ src/main/kotlin/pl/jpodeszwik/aoc2023/Util.kt
@@ -5,6 +5,18 @@ import kotlin.system.measureTimeMillis
 class Util {
 }
 
-fun loadFile(name: String) = Util::class.java.getResource(name)!!.readText().split("\n")
+fun loadEntireFile(name: String) = Util::class.java.getResource(name)!!.readText()
+
+fun loadFile(name: String) = loadEntireFile(name).split("\n")
+
+fun flipBit(num: Int, bit: Int): Int {
+    val lowestBit = (num shr bit) and 1
+
+    return if (lowestBit == 1) {
+        num - (1 shl bit)
+    } else {
+        num + (1 shl bit)
+    }
+}
 
 fun measureTimeSeconds(block: () -> Unit) = measureTimeMillis { block() }.also { println("time: ${it / 1000}s") }
