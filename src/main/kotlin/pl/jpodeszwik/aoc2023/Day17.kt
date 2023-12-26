404d1a48e8180fdcafded87fbd71ca94e678aa48
Jakub Podeszwik <jakubpodeszwik@gmail.com>
2023-12-26 16:00:28 +0100

    Day17 part2

--- src/main/kotlin/pl/jpodeszwik/aoc2023/Day17.kt
+++ src/main/kotlin/pl/jpodeszwik/aoc2023/Day17.kt
@@ -19,15 +19,10 @@ data class Cost(val costs: MutableMap<DirStreak, Int>) {
     fun value() = costs.values.minOrNull() ?: MAX_VALUE
 }
 
-private fun part1(input: List<String>) {
-    val coords = mutableMapOf<MapCoord, Int>()
+private fun part1(graph: Map<MapCoord, Int>) {
     val dist = mutableMapOf<MapCoord, Cost>()
-    input.forEachIndexed { rowNum, row ->
-        row.forEachIndexed { colNum, v ->
-            val coord = MapCoord(rowNum, colNum)
-            coords[coord] = parseInt(v.toString())
-            dist[coord] = Cost(mutableMapOf())
-        }
+    graph.keys.forEach {
+        dist[it] = Cost(mutableMapOf())
     }
 
     val initial = MapCoord(0, 0)
@@ -41,17 +36,17 @@ private fun part1(input: List<String>) {
     )
 
     val queue = PriorityQueue<MapCoord>(Comparator.comparing { dist[it]!!.value() })
-    queue.addAll(coords.keys)
+    queue.addAll(graph.keys)
 
     while (queue.isNotEmpty()) {
         val current = queue.poll()
         val currentDist = dist[current]!!
         current.neighbours()
-            .filter { coords.containsKey(it.second) }
+            .filter { graph.containsKey(it.second) }
             .forEach { n ->
                 val (direction, neighbour) = n
                 val neighbourDist = dist[neighbour]!!
-                val edgeCost = coords[neighbour]!!
+                val edgeCost = graph[neighbour]!!
 
                 currentDist.costs
                     .filter { direction.opposite() != it.key.direction }
@@ -76,7 +71,86 @@ private fun part1(input: List<String>) {
     println(dist[maxKey]!!.value())
 }
 
+private fun part2(graph: Map<MapCoord, Int>) {
+    val dist = mutableMapOf<MapCoord, Cost>()
+
+    graph.keys.forEach {
+        dist[it] = Cost(mutableMapOf())
+    }
+
+    val initial = MapCoord(0, 0)
+    dist[initial] = Cost(
+        mutableMapOf(
+            DirStreak(Direction.DOWN, 0) to 0,
+            DirStreak(Direction.RIGHT, 0) to 0,
+        )
+    )
+
+    val queue = PriorityQueue<MapCoord>(Comparator.comparing { dist[it]!!.value() })
+    queue.addAll(graph.keys)
+
+    while (queue.isNotEmpty()) {
+        val current = queue.poll()
+        val currentDist = dist[current]!!
+        current.neighbours()
+            .filter { graph.containsKey(it.second) }
+            .forEach { n ->
+                val (direction, neighbour) = n
+                val neighbourDist = dist[neighbour]!!
+                val edgeCost = graph[neighbour]!!
+
+                currentDist.costs
+                    .filter { direction.opposite() != it.key.direction }
+                    .forEach {
+                        if (direction == it.key.direction) {
+                            val streak = it.key.streak + 1
+                            if (streak <= 10) {
+                                val dirStreak = DirStreak(
+                                    direction,
+                                    streak
+                                )
+                                val updated = neighbourDist.newDirStreak(dirStreak, it.value + edgeCost)
+                                if (updated) {
+                                    queue.remove(neighbour)
+                                    queue.add(neighbour)
+                                }
+                            }
+                        } else {
+                            if (it.key.streak >= 4) {
+                                val dirStreak = DirStreak(
+                                    direction,
+                                    1
+                                )
+                                val updated = neighbourDist.newDirStreak(dirStreak, it.value + edgeCost)
+                                if (updated) {
+                                    queue.remove(neighbour)
+                                    queue.add(neighbour)
+                                }
+                            }
+                        }
+                    }
+            }
+    }
+
+    val maxKey = dist.keys.maxBy { it.row + it.col }
+    println(dist[maxKey]!!.value())
+}
+
+private fun buildGraph(input: List<String>): MutableMap<MapCoord, Int> {
+    val coords = mutableMapOf<MapCoord, Int>()
+    input.forEachIndexed { rowNum, row ->
+        row.forEachIndexed { colNum, v ->
+            val coord = MapCoord(rowNum, colNum)
+            coords[coord] = parseInt(v.toString())
+
+        }
+    }
+    return coords
+}
+
 fun main() {
     val input = loadFile("/aoc2023/input17")
-    part1(input)
+    val graph = buildGraph(input)
+    part1(graph)
+    part2(graph)
 }
